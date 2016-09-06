77d519890e990689ee0ec5abb044d7d9d5bbe02a
Guy Khmel <guykhmel@gmail.com>
2016-09-06 18:57:10 +0300

    Signed-off-by: Guy Khmel <guykhmel@gmail.com>

--- RoomateSite/app/bills/bill.routing.ts
+++ RoomateSite/app/bills/bill.routing.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule} from '@angular/router';
 
 import { BillListComponent } from './bill-list.component';
 import { BillDetailComponent } from './bill-detail.component';
+import { BillGraphComponent} from './bill-graph.component';
 
 export const billRoutes: Routes = [
     { path: 'bills', component: BillListComponent },
-    { path: 'bill/:id', component: BillDetailComponent }
+    { path: 'bill/:id', component: BillDetailComponent },
+    { path: 'billsGraph', component: BillGraphComponent }
 ];
 
 export const billRouting: ModuleWithProviders =
